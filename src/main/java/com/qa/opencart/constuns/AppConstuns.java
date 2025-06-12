2fcc8091de247366ee78339e737736d636e1f954
Rajesh <rajesh.badhe33@gmail.com>
2025-06-12 23:55:44 +0530

    removed hard coded values.

--- src/main/java/com/qa/opencart/constuns/AppConstuns.java
+++ src/main/java/com/qa/opencart/constuns/AppConstuns.java
@@ -3,6 +3,12 @@ package com.qa.opencart.constuns;
 public class AppConstuns {
 
     public static final String LOGIN_PAGE_TITLE = "Your Store";
+    public static final String CONFIG_FILE_PATH = "./src/test/resources/config/config.properties";
+    public static final String CHROMIUM = "chromium";
+    public static final String FIREFOX ="firefox";
+    public static final String SAFARI ="safari";
+    public static final String CHROME ="chrome";
+
 
 
 }
