715cf29b33a8638b85faac4aebd989503182c0be
Rajesh <rajesh.badhe33@gmail.com>
2025-06-20 17:28:40 +0530

    Chaintest report Integration with Playwright

--- src/test/java/com/qa/opencart/baseTest/BaseTest.java
+++ src/test/java/com/qa/opencart/baseTest/BaseTest.java
@@ -5,6 +5,8 @@ import com.qa.opencart.factory.PlaywrightFactory;
 import com.qa.opencart.pages.*;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 import java.io.IOException;
 import java.util.Properties;
@@ -20,11 +22,18 @@ public class BaseTest {
     protected AddressBookPage addressBookPage;
     protected AddNewAddressPage addNewAddressPage;
 
-
+    @Parameters({"browsers"})
     @BeforeTest
-    public void setup() throws IOException {
+    public void setup(@Optional String browserName) throws IOException {
+
         playwrightFactory = new PlaywrightFactory();
         prop = playwrightFactory.initProp();
+
+        //check if browser param is coming from testng.xml
+        if (browserName != null) {
+            prop.setProperty("browser", browserName);
+        }
+
         page = playwrightFactory.initBrowser(prop);
         homePage = new HomePage(page);
     }
